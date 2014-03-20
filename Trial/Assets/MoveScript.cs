753b7002842fbe61106f22b83fc555d10f1c36fb
Romaine Herrera <pizza_aldwyn@yahoo.com>
2014-03-20 16:54:38 -0400

    Yay

---
+++ Trial/Assets/MoveScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+	void Update () {
+
+
+		if (Input.GetMouseButtonDown(0)) {
+			float jumpSpeed = 5.0f;
+						//transform.Translate (0, 5, 0);
+			rigidbody.velocity = new Vector3(0,0,0);
+			rigidbody.AddForce(new Vector3(0, 320, 0) * 1.4f, ForceMode.Force);
+			//this.rigidbody2D.velocity.y = 5;
+		}
+		if (Input.GetTouch (0).phase == TouchPhase.Began) {
+			this.rigidbody.AddForce(Vector3.up * 100.0f);
+		}
+	}
+}
