397bbafbb544bbd3cb3e56f818ab406775e21dd5
Romaine Herrera <pizza_aldwyn@yahoo.com>
2014-03-21 00:10:05 -0400

    2D Version Flappy Bird Backwards :)

---
+++ 2d Version Trial/Assets/Scripts/Beams.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Beams : MonoBehaviour {
+
+	public Vector2 velocity = new Vector2(4, 0);
+
+	// Use this for initialization
+	void Start()
+	{
+		rigidbody2D.velocity = velocity;
+
+		float randomRange = Random.Range (-3.5f, 3.5f);
+		float random = Random.value;
+
+		transform.position = new Vector3(transform.position.x, transform.position.y - randomRange * random, transform.position.z);
+	}
+}
---
+++ 2d Version Trial/Assets/Scripts/Generate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generate : MonoBehaviour {
+
+	public GameObject beams;
+	
+	// Use this for initialization
+	void Start()
+	{
+		InvokeRepeating("CreateObstacle", 1f, 2f);
+	}
+	
+	void CreateObstacle()
+	{
+		Instantiate(beams);
+	}
+}
