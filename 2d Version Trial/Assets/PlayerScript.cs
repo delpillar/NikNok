26065e5a12b32e6a37ffba151d0568e44827981d
Romaine Herrera <pizza_aldwyn@yahoo.com>
2014-03-20 21:21:03 -0400

    It dies and restarts when hit the platforms

---
+++ 2d Version Trial/Assets/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			rigidbody2D.velocity = Vector2.zero;
+			rigidbody2D.AddForce(new Vector2(0, 300));
+				}
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		Die();
+	}
+
+	void Die()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+}
