f92bddfbc0fedcd060e84d535f3d692f78155b64
Romaine Herrera <pizza_aldwyn@yahoo.com>
2014-03-20 21:11:37 -0400

    clearing

--- Trial/Assets/MoveScript.cs
+++ Trial/Assets/MoveScript.cs
@@ -9,12 +9,13 @@ public class MoveScript : MonoBehaviour {
 		if (Input.GetMouseButtonDown(0)) {
 			float jumpSpeed = 5.0f;
 						//transform.Translate (0, 5, 0);
-			rigidbody.velocity = new Vector3(0,0,0);
-			rigidbody.AddForce(new Vector3(0, 320, 0) * 1.4f, ForceMode.Force);
+			rigidbody.velocity = new Vector2(0,0);
+			rigidbody.AddForce(new Vector2(0, 320) * 1.4f, ForceMode.Force);
 			//this.rigidbody2D.velocity.y = 5;
 		}
 		if (Input.GetTouch (0).phase == TouchPhase.Began) {
-			this.rigidbody.AddForce(Vector3.up * 100.0f);
+			rigidbody.velocity = new Vector2(0,0);
+			rigidbody.AddForce(new Vector2(0, 320) * 1.4f, ForceMode.Force);
 		}
 	}
 }
