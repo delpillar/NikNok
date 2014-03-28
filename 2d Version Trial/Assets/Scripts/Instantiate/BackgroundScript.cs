22a59d6e6000bda6331523dfe4e040f06c8b4363
Romaine Herrera <pizza_aldwyn@yahoo.com>
2014-03-28 15:52:40 -0400

    Background and Organized Folders

---
+++ 2d Version Trial/Assets/Scripts/Instantiate/BackgroundScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScript : MonoBehaviour {
+
+	public GameObject bg1;
+	public GameObject bg2;
+
+
+	// Use this for initialization
+	void Start () {
+
+		Instantiate (bg1);
+		Instantiate (bg2);
+	}
+}
---
+++ 2d Version Trial/Assets/Scripts/Logic/BG/bg1script.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bg1script : MonoBehaviour {
+
+	public Vector2 velocity = new Vector2(-4, 0);
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.velocity = velocity;
+		transform.position = new Vector3(0, 0, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 temp = rigidbody2D.transform.position;
+
+		if (temp.x <= -10) {
+			temp.x = 10;
+		}
+
+		rigidbody2D.transform.position = temp;
+	}
+}
