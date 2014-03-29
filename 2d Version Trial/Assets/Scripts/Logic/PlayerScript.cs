adac795f3f1252a3385182d3b357658350b80a8a
delpillar <knivex27@hotmail.com>
2014-03-29 17:06:39 -0400

    Tested on Nexus 5
    Adjusted camera to fit in Nexus 5 resolution. Changed beam sprite. Good
    job tol!

--- 2d Version Trial/Assets/Scripts/Logic/PlayerScript.cs
+++ 2d Version Trial/Assets/Scripts/Logic/PlayerScript.cs
@@ -2,17 +2,19 @@
 using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
-	
+
+	public Vector2 jumpSpeed = new Vector2(0,30);
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
 			rigidbody2D.velocity = Vector2.zero;
-			rigidbody2D.AddForce(new Vector2(0, 300));
+			rigidbody2D.AddForce(new Vector2(0,jumpSpeed.y*10));
 				}
 
 		if (Input.GetTouch (0).phase == TouchPhase.Began) {
 			rigidbody2D.velocity = Vector2.zero;
-			rigidbody2D.AddForce(new Vector2(0, 30));
+			rigidbody2D.AddForce(jumpSpeed);
 		}
 	}
 
