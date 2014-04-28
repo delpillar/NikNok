dbaffa7c42649396e3799c43e5a073ce6241286a
Romaine Herrera <pizza_aldwyn@yahoo.com>
2014-04-28 00:28:58 -0400

    Removed Smoke | Add Jump/Fly/Booster Animation

--- 2d Version Trial/Assets/Scripts/Logic/PlayerScript.cs
+++ 2d Version Trial/Assets/Scripts/Logic/PlayerScript.cs
@@ -5,17 +5,25 @@ public class PlayerScript : MonoBehaviour {
 
 	public Vector2 jumpSpeed = new Vector2(0,30);
 
+	Animator anim;
+
+	void Start() {
+		anim = GetComponent<Animator> ();
+
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
 			rigidbody2D.velocity = Vector2.zero;
-			rigidbody2D.AddForce(new Vector2(0,jumpSpeed.y*10));
+			rigidbody2D.AddForce(new Vector2(0,jumpSpeed.y * 10));
+			anim.SetTrigger("Jump");
 				}
 
-		if (Input.GetTouch (0).phase == TouchPhase.Began) {
-			rigidbody2D.velocity = Vector2.zero;
-			rigidbody2D.AddForce(jumpSpeed);
-		}
+		//if (Input.GetTouch (0).phase == TouchPhase.Began) {
+		//	rigidbody2D.velocity = Vector2.zero;
+		//	rigidbody2D.AddForce(jumpSpeed);
+		//}
 	}
 
 	void OnCollisionEnter2D(Collision2D other)
